98a67e62cd552bd624b330f408940198415f92a5
Matthew McIntyre <matthewtfm@gmail.com>
2019-10-14 17:32:39 -0400

    setting up final branch

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,69 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
+  pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage = false;
+  errorMessage = '';
 
-    pageTitle: string = 'Product List';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
-    showImage: boolean = false;
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
 
-    private _listFilter: string = 'cart';
-    public get listFilter(): string {
-        return this._listFilter;
-    }
-    public set listFilter(value: string) {
-        this._listFilter = value;
-        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
-    }
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
 
-    // use any[] as the data type anytime we don't know, or don't care what the data type is.
-    filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
+  constructor(private productService: ProductService) {
 
-    constructor() {
+  }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List: ' + message;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
         this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
-
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) =>
-            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
-    }
-
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
-
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
-
+      },
+      error: err => this.errorMessage = err
+    });
+  }
 }
--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'pm-star',
-    templateUrl: './star.component.html',
-    styleUrls: ['./star.component.css']
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges {
-    rating: number = 4;
-    starWidth: number;
+  @Input() rating = 0;
+  starWidth = 0;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
 
-    ngOnChanges(): void {
-        this.starWidth = this.rating * 75 / 5;
-    }
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 75 / 5;
+  }
 
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
 }
