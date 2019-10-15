2fcd43c6cb3f3623b34f0ab21db407dc688bd0c0
Matthew McIntyre <matthewtfm@gmail.com>
2019-10-15 14:11:17 -0400

    progress 10-15-19

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 
@@ -8,24 +9,23 @@ import { ProductService } from './product.service';
     styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     errorMessage: string;
 
-    // private _listFilter: string = 'cart';
-    public get listFilter(): string {
+    _listFilter: string;
+    get listFilter(): string {
         return this._listFilter;
     }
-    public set listFilter(value: string) {
+    set listFilter(value: string) {
         this._listFilter = value;
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
     // use any[] as the data type anytime we don't know, or don't care what the data type is.
-    filteredProducts: IProduct[];
+    filteredProducts: IProduct[] = [];
     products: IProduct[] = [];
 
     constructor(private productService: ProductService) {}
@@ -46,10 +46,11 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         this.productService.getProducts().subscribe({
-            next: products => this.products = products,
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
             error: err => this.errorMessage = err
         });
-        this.filteredProducts = this.products;
     }
-
 }
