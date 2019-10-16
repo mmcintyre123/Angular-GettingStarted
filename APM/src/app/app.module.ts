54c79baf0e99a5a9a0fff9042d3a5095c8dc95b1
Matthew McIntyre <matthewtfm@gmail.com>
2019-10-16 10:25:01 -0400

    progress 10-15-19

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,25 +1,39 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { ProductDetailGuard } from './products/product-detail.guard';
 import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { RouterModule } from "@angular/router";
 import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent
-],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        ConvertToSpacesPipe,
+        StarComponent,
+        ProductDetailComponent,
+        WelcomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent },
+            { path: 'products/:id',
+              canActivate: [ProductDetailGuard],
+              component: ProductDetailComponent },
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+        ])
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
